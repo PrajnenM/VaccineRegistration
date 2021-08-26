a711c966254dbf956f7771d59382e63ee50562f9
PrajnenM <prajnen@gmail.com>
2021-08-26 14:47:07 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace VaccineRegistration
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textFirstName = new System.Windows.Forms.TextBox();
+            this.textSurname = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textID = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.GendersCB = new System.Windows.Forms.ComboBox();
+            this.ProvinceCB = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(232, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Surname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "FirstName";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(232, 287);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(233, 343);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Province";
+            // 
+            // textFirstName
+            // 
+            this.textFirstName.Location = new System.Drawing.Point(395, 227);
+            this.textFirstName.Name = "textFirstName";
+            this.textFirstName.Size = new System.Drawing.Size(171, 23);
+            this.textFirstName.TabIndex = 8;
+            // 
+            // textSurname
+            // 
+            this.textSurname.Location = new System.Drawing.Point(395, 166);
+            this.textSurname.Name = "textSurname";
+            this.textSurname.Size = new System.Drawing.Size(171, 23);
+            this.textSurname.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(327, 398);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 40);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textID
+            // 
+            this.textID.Location = new System.Drawing.Point(395, 112);
+            this.textID.Name = "textID";
+            this.textID.Size = new System.Drawing.Size(171, 23);
+            this.textID.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(189, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(429, 44);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Covid-19 Vaccine Registration";
+            // 
+            // GendersCB
+            // 
+            this.GendersCB.FormattingEnabled = true;
+            this.GendersCB.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Other"});
+            this.GendersCB.Location = new System.Drawing.Point(395, 287);
+            this.GendersCB.Name = "GendersCB";
+            this.GendersCB.Size = new System.Drawing.Size(171, 23);
+            this.GendersCB.TabIndex = 14;
+            // 
+            // ProvinceCB
+            // 
+            this.ProvinceCB.FormattingEnabled = true;
+            this.ProvinceCB.Items.AddRange(new object[] {
+            "Gauteng",
+            "Free State",
+            "North West",
+            "Northern Cape",
+            "Eastern Cape",
+            "Western Cape",
+            "Limpopo",
+            "Mpumalanga",
+            "Kwa Zulu Natal"});
+            this.ProvinceCB.Location = new System.Drawing.Point(395, 343);
+            this.ProvinceCB.Name = "ProvinceCB";
+            this.ProvinceCB.Size = new System.Drawing.Size(171, 23);
+            this.ProvinceCB.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ProvinceCB);
+            this.Controls.Add(this.GendersCB);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textID);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textSurname);
+            this.Controls.Add(this.textFirstName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textFirstName;
+        private System.Windows.Forms.TextBox textSurname;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textID;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox GendersCB;
+        private System.Windows.Forms.ComboBox ProvinceCB;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VaccineRegistration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+
+            {
+                String str = "Data Source=.;Initial Catalog=CTU_8025;Integrated Security=True";
+
+            String queryTwo = "insert into RegisteredUsers(Id, Surname, Firstname, Gender, Province)" +
+                "Values('" + textID.Text + "','" + textSurname.Text + "','" + textFirstName.Text + "','" + GendersCB.Text + "','" + ProvinceCB.Text + "')";
+
+            String query = "select * from data";
+
+            SqlConnection con = new SqlConnection(str);
+
+            SqlCommand cmd = new SqlCommand(queryTwo, con);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+
+
+
+            MessageBox.Show("Thank you for registering to get the COVID-19 Vaccine");
+
+            con.Close();
+            }
+            catch (Exception es)
+            {
+                MessageBox.Show(es.Message);
+            }
+        }
+    }
+}
